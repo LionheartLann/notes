751274cda184309a2f343fc2a68f9daf4f488dbc
xingcheng.xia@apix.cn <xingcheng.xia@apix.cn>
2018-08-06 17:00:40 +0800

    Segmentation fault: 11

---
+++ PrintListReversingly.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stack>
+using std::stack;
+using std::cout;
+using std::endl;
+struct ListNode{
+    int value;
+    ListNode* next;
+};
+//void PrintListReversingly(ListNode* Head){
+//    ListNode* p = Head;
+//    stack<ListNode*> nodes;
+//    while(p!=NULL){
+//        nodes.push(p);
+//        p = p->next;
+//    }
+//    while(!nodes.empty()){
+//        p = nodes.top();
+//        cout<<p->value<<endl;
+//        nodes.pop();
+//    }
+//}
+int main(){
+    ListNode* Head = NULL;
+    ListNode* p = Head;
+    int i = 0;
+    while(i<=10){
+        ListNode* p1 = new ListNode();
+        p1->value = i;
+        p1->next = NULL;
+        p->next = p1;
+        p = p->next;
+        i++;
+        cout<<i<<endl;
+    }
+    //PrintListReversingly(Head);
+    return 0;
+    
+}
