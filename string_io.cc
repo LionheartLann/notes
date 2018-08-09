f3ad08970dfc82ead4d60b4d4d898808b6806f8b
xingcheng.xia@apix.cn <xingcheng.xia@apix.cn>
2018-08-09 14:32:09 +0800

    print bigger string

--- string_io.cc
+++ string_io.cc
@@ -7,7 +7,16 @@ using std::endl;
 using std::string;
 
 int main(){
-    string s;
-    while(getline(cin, s))
-        cout<<s<<endl;
+    string s1, s2;
+    //while(getline(cin, s))
+        //cout<<s<<endl;
+    cin>>s1>>s2;
+    if(s1>s2){
+        cout<<s1<<endl;
+    }else if(s1<s2){
+        cout<<s2<<endl;
+    }else{
+        cout<<"=="<<endl;
+    }
+    return 0;
 }
