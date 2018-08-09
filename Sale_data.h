a4bf0929891a67fb5af2392b5cd3eed03600f872
xingcheng.xia@apix.cn <xingcheng.xia@apix.cn>
2018-08-09 14:24:30 +0800

    string read cin words

--- Sale_data.h
+++ Sale_data.h
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string>
-using std::cin;
-using std::cout;
-using std::cerr;
-using std::endl;
-using std::string;
+// Note: should not use "using" in head file! To  avoid collision
+//using std::cin;
+//using std::cout;
+//using std::cerr;
+//using std::endl;
+//using std::string;
 
 struct Sale_data{
-    string bookNo;
+    std::string bookNo;
     unsigned units_sold = 0;
     double revenue = 0.0;
 };
